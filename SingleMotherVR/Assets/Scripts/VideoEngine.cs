a0d83dd0f7b27e4a0e3cb4013552845171d30fbe
Ashot Danielyan <ashot111997@gmail.com>
2020-08-14 14:48:45 +0400

    creating video scene

---
+++ SingleMotherVR/Assets/Scripts/VideoEngine.cs
@@ -0,0 +1,32 @@
+﻿using RenderHeads.Media.AVProVideo;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VideoEngine : MonoBehaviour
+{
+    public MediaPlayer VideoPlayer;
+
+    public Button CloseBtn;
+
+    private void Awake()
+    {
+        CloseBtn.onClick.AddListener(Close);
+    }
+
+    void Start()
+    {
+        PlayVideo();
+    }
+
+    void PlayVideo() {
+        string videoUrl = PlayerPrefs.GetString("VideoUrl");
+        VideoPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.AbsolutePathOrURL, videoUrl, true);
+    }
+
+    void Close() {
+        SceneManager.LoadScene("Environment");
+    }
+}
