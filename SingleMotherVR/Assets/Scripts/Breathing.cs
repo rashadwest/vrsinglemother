778f5306bbc1bd3b7f3cdd4111ed812eb646702f
Ashot Danielyan <ashot111997@gmail.com>
2020-09-01 20:01:40 +0400

    added new trees and breathing system

---
+++ SingleMotherVR/Assets/Scripts/Breathing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breathing : MonoBehaviour
+{
+    public ParticleSystem FrostEffect;
+
+    public AudioSource BreathingSoundEffect;
+
+    private void Start()
+    {
+        BreathingSoundEffect.Pause();
+        StartCoroutine(StartBreathing());
+    }
+
+    IEnumerator StartBreathing()
+    {
+        BreathingSoundEffect.Play();
+        yield return new WaitForSeconds(1);
+        FrostEffect.Play();
+        yield return new WaitForSeconds(5);
+        StartCoroutine(StartBreathing());
+    }
+}
