6f37105ca183735e728bf10cf41618a58f6db7ca
Ashot Danielyan <ashot111997@gmail.com>
2020-09-11 16:50:36 +0400

    fixed breathing indoor outdoor

--- SingleMotherVR/Assets/Scripts/Breathing.cs
+++ SingleMotherVR/Assets/Scripts/Breathing.cs
@@ -8,8 +8,16 @@ public class Breathing : MonoBehaviour
 
     public AudioSource BreathingSoundEffect;
 
+    public Material Material;
+
+    public Texture OutFrost;
+    public Texture InFrost;
+
+    private Transform Target;
+
     private void Start()
     {
+        Target = Camera.main.GetComponent<Transform>();
         BreathingSoundEffect.Pause();
         StartCoroutine(StartBreathing());
     }
@@ -21,5 +29,23 @@ public class Breathing : MonoBehaviour
         FrostEffect.Play();
         yield return new WaitForSeconds(5);
         StartCoroutine(StartBreathing());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform != Target)
+            return;
+
+        Material.mainTexture = InFrost;
+        BreathingSoundEffect.volume = 0.2f;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform != Target)
+            return;
+
+        Material.mainTexture = OutFrost;
+        BreathingSoundEffect.volume = 0.5f;
     }
 }
