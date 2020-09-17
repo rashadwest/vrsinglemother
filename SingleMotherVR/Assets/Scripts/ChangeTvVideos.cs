21903b2d04cc239a57431fc17839d8b594a15132
Ashot Danielyan <ashot111997@gmail.com>
2020-09-17 16:47:56 +0400

    Created tv video slideshow

---
+++ SingleMotherVR/Assets/Scripts/ChangeTvVideos.cs
@@ -0,0 +1,32 @@
+﻿using RenderHeads.Media.AVProVideo;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTvVideos : MonoBehaviour
+{
+    public MediaPlayer VideoPlayer;
+
+    public List<string> VideoUrls = new List<string>();
+
+    void Start()
+    {
+        StartCoroutine(PlayVideos());
+    }
+
+    IEnumerator PlayVideos() {
+        foreach (string url in VideoUrls)
+        {
+            VideoPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.RelativeToStreamingAssetsFolder, url);
+            yield return new WaitForSeconds(1);
+            int waitTime = (int)VideoPlayer.Info.GetDurationMs() / 1000;
+            yield return new WaitForSeconds(waitTime - 1);
+        }
+        
+        StartCoroutine(PlayVideos());
+    }
+
+    void Update()
+    {
+    }
+}
