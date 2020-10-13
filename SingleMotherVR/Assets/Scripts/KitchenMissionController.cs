7ff4df880046e662cb206ba448441cf412be214a
Ashot Danielyan <ashot111997@gmail.com>
2020-10-13 17:15:05 +0400

    working on kitchen mission

---
+++ SingleMotherVR/Assets/Scripts/KitchenMissionController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenMissionController : MonoBehaviour
+{
+    [Header("Start")]
+    public GameObject Particle;
+    public Transform Avatar;
+    public CharacterController AvatarController;
+    private Transform Target;
+
+    private void Start()
+    {
+        Target = Camera.main.transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other != Target.transform)
+            return;
+
+        Debug.LogError("Start Kitchen Mission");
+    }
+}
