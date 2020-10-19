e99b8ceda58468ba664080ebe6eb6eb4ca833452
Ashot Danielyan <ashot111997@gmail.com>
2020-10-19 16:45:02 +0400

    starting yard mission

---
+++ SingleMotherVR/Assets/Scripts/YardWorkController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YardWorkController : MonoBehaviour
+{
+    [Header("General")]
+    private Transform Target;
+    public GameObject MissionParticle;
+
+    [Header("Label Part")]
+    public GameObject LabelCanvas;
+    public Text LabelText;
+
+    private void Start()
+    {
+        Target = Camera.main.transform;
+        LabelCanvas.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform != Target)
+            return;
+
+        StartMission();
+    }
+
+    void StartMission() {
+        MissionParticle.SetActive(false);
+        LabelCanvas.SetActive(true);
+    }
+}
