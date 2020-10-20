a25c86d7217a6d0e2af580bc3c0f135a36291693
Ashot Danielyan <ashot111997@gmail.com>
2020-10-20 17:11:49 +0400

    continue work on  outdoor mission

--- SingleMotherVR/Assets/Scripts/YardWorkController.cs
+++ SingleMotherVR/Assets/Scripts/YardWorkController.cs
@@ -8,15 +8,23 @@ public class YardWorkController : MonoBehaviour
     [Header("General")]
     private Transform Target;
     public GameObject MissionParticle;
+    public YardWorkEndTrigger MissonEndTrigger;
+    public BoxCollider MissonStartTrigger;
 
     [Header("Label Part")]
     public GameObject LabelCanvas;
     public Text LabelText;
 
+    private void Awake()
+    {
+        MissonEndTrigger.Callback += EndMission;
+    }
+
     private void Start()
     {
         Target = Camera.main.transform;
         LabelCanvas.SetActive(false);
+        MissonEndTrigger.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,7 +36,36 @@ public class YardWorkController : MonoBehaviour
     }
 
     void StartMission() {
+        MissonStartTrigger.enabled = false;
         MissionParticle.SetActive(false);
+        LabelCanvas.GetComponent<Animator>().Play("CanvasGroupFadeIn");
         LabelCanvas.SetActive(true);
+        MissonEndTrigger.gameObject.SetActive(true);
+
+        StartCoroutine(PlaySubtitles());
+    }
+
+    IEnumerator PlaySubtitles()
+    {
+        LabelText.text = "";
+        LabelText.color = Color.white;
+        yield return new WaitForSeconds(1);
+        LabelText.text = "Take the broom";
+    }
+
+    void EndMission() {
+        MissionParticle.SetActive(true);
+        MissonStartTrigger.enabled = true;
+        LabelText.text = "Mission End";
+        LabelText.color = Color.red;
+        StartCoroutine(HideCanvas());
+    }
+
+    IEnumerator HideCanvas()
+    {
+        yield return new WaitForSeconds(0.6f);
+        LabelCanvas.GetComponent<Animator>().Play("CanvasGroupFadeOut");
+        yield return new WaitForSeconds(0.6f);
+        LabelCanvas.SetActive(false);
     }
 }
---
+++ SingleMotherVR/Assets/Scripts/YardWorkEndTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class YardWorkEndTrigger : MonoBehaviour
+{
+    private Transform Target;
+
+    public Action Callback;
+
+    private void Start()
+    {
+        Target = Camera.main.transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform != Target)
+            return;
+
+        Callback.Invoke();
+    }
+}
