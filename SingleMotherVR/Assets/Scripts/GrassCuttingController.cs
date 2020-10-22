27ba4d1a95a8e2a90c2e22547a54b9736ae3622a
Ashot Danielyan <ashot111997@gmail.com>
2020-10-22 15:31:57 +0400

    finished lawn mower scene

---
+++ SingleMotherVR/Assets/Scripts/GrassCuttingController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassCuttingController : MonoBehaviour
+{
+    private Transform Target;
+
+    private void Start()
+    {
+        StartCoroutine(FindTarget());
+    }
+
+    IEnumerator FindTarget() {
+        yield return new WaitForSeconds(1);
+        Target = GameObject.FindGameObjectWithTag("LawnMower").transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
--- SingleMotherVR/Assets/Scripts/LawnMowerMissionController.cs
+++ SingleMotherVR/Assets/Scripts/LawnMowerMissionController.cs
@@ -10,12 +10,18 @@ public class LawnMowerMissionController : MonoBehaviour
     public CharacterController AvatarController;
     public Vector3 StartingPosition;
 
+    public AudioSource LawnMower1;
+    public AudioSource LawnMower2;
+
     private Transform Cam;
     private GameObject LawnMawer;
 
     private void Start()
     {
         Cam = Camera.main.transform;
+
+        LawnMower1.enabled = false;
+        LawnMower2.enabled = false;
 
         StartMission();
     }
@@ -24,7 +30,25 @@ public class LawnMowerMissionController : MonoBehaviour
         AvatarController.enabled = false;
         Avatar.localPosition = StartingPosition;
         AvatarController.enabled = true;
-
         LawnMawer = Instantiate(LawnMawerPrefab, LawnMawerParent);
+        StartCoroutine(PlayAudio());
+    }
+
+    private void Update()
+    {
+        if (LawnMawer == null)
+            return;
+
+        LawnMawer.transform.localEulerAngles = new Vector3(0, Cam.localEulerAngles.y, 0);
+
+
+    }
+
+    IEnumerator PlayAudio() {
+        LawnMower1.enabled = true;
+        yield return new WaitForSeconds(LawnMower1.clip.length - 0.5f);
+        LawnMawer.transform.GetChild(0).GetComponent<Animator>().Play("LawnMower");
+        LawnMower1.enabled = false;
+        LawnMower2.enabled = true;
     }
 }
