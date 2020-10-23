bc57a2c7868b6608c458bef776e6880c1df5b49a
Ashot Danielyan <ashot111997@gmail.com>
2020-10-23 17:43:14 +0400

    creating post production effect

---
+++ SingleMotherVR/Assets/Scripts/PostProcessingExperiment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Rendering;
+
+public class PostProcessingExperiment : MonoBehaviour
+{
+    public PostProcessVolume Volume;
+
+    private ColorGrading Color;
+    private Vignette Vignate;
+
+    private void Start()
+    {
+        Volume.profile.TryGetSettings(out Color);
+        Volume.profile.TryGetSettings(out Vignate);
+        Color.saturation.value = 0;
+        Vignate.intensity.value = 0;
+        Vignate.smoothness.value = 0;
+    }
+
+    private IEnumerator Anim()
+    {
+        yield return new WaitForSeconds(0);
+        Color.saturation.value = Mathf.Lerp(Color.saturation.value, -100, 1f * Time.deltaTime);
+        Vignate.intensity.value = Mathf.Lerp(Vignate.intensity.value, 0.3f, 1f * Time.deltaTime);
+        Vignate.smoothness.value = Mathf.Lerp(Vignate.smoothness.value, 1, 1f * Time.deltaTime);
+    }
+}
