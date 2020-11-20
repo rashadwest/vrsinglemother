9cf1a2c8d3559d7bb69d51ef9f2ad2423fd3ee88
Ashot Danielyan <ashot111997@gmail.com>
2020-11-20 18:38:28 +0400

    starting new room mission.

--- SingleMotherVR/Assets/Oculus/VR/Scripts/Util/OVRScreenFade.cs
+++ SingleMotherVR/Assets/Oculus/VR/Scripts/Util/OVRScreenFade.cs
@@ -24,7 +24,7 @@ using System.Collections; // required for Coroutines
 public class OVRScreenFade : MonoBehaviour
 {
     [Tooltip("Fade duration")]
-	public float fadeTime = 2.0f;
+	public float fadeTime = 1.0f;
 
     [Tooltip("Screen color at maximum fade")]
 	public Color fadeColor = new Color(0.01f, 0.01f, 0.01f, 1.0f);
---
+++ SingleMotherVR/Assets/Scripts/RoomMissionController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMissionController : MonoBehaviour
+{
+    private Transform Target;
+    private bool IsStarted = false;
+
+    public GameObject MissionParticle;
+    public CharacterController AvatarController;
+    public Transform Avatar;
+    public OVRScreenFade ScreenFade;
+    public Vector3 StartingPos;
+    public Vector3 StartingRot;
+        
+    void Start()
+    {
+        Target = Camera.main.transform;
+        IsStarted = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform != Target)
+            return;
+
+        if (!IsStarted)
+        {
+            StartCoroutine(StartMission());
+        }
+        else
+        {
+            EndMission();
+        }
+    }
+
+    IEnumerator StartMission() {
+        IsStarted = true;
+        MissionParticle.SetActive(false);
+        AvatarController.enabled = false;
+        ScreenFade.FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        Avatar.localPosition = StartingPos;
+        Avatar.localRotation = Quaternion.Euler(StartingRot);
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(ScreenFade.Fade(1, 0, value => {
+            AvatarController.enabled = true;
+        }));
+    }
+
+    void EndMission() {
+        IsStarted = false;
+        MissionParticle.SetActive(true);
+    }
+}
