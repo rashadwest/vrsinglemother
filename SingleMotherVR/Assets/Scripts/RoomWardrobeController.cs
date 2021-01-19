bf01be60edf667cd9dcb97a96387b90e82c05c30
Ashot Danielyan <ashot111997@gmail.com>
2021-01-19 17:06:04 +0400

    added room cube

---
+++ SingleMotherVR/Assets/Scripts/RoomWardrobeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomWardrobeController : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Hat")
+        {
+            //Hat
+        }
+        else if (other.name == "Backpack")
+        {
+            //Backpack
+        }
+        else if (other.name == "Shirt")
+        {
+            //Shirt
+        }
+        else if (other.name == "Shoes")
+        {
+            //Shoes
+        }
+    }
+}
